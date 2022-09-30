910e0843cc4e36aa2a4c753a5ee025a032fddd7f
yorutoitsuki <thouxcv@gmail.com>
2022-09-30 16:05:15 +0900

    Update processManagement.sql

--- 220923_Process_ManageMent/src/main/webapp/processManagement.sql
+++ 220923_Process_ManageMent/src/main/webapp/processManagement.sql
@@ -86,9 +86,9 @@ join tbl_product using(p_code) order by w_workno
 
 
 --14P SQL 작업공정조회
-select w_workno, P_code, p_name, p_p1, p_p2, p_p3, p_p4, p_p5, p_p6,
-w_lastdate, w_lasttime from tbl_worklist full outer join tbl_process using(w_workno) 
-join tbl_product using(p_code) order by w_workno
+select w_workno, nvl(P_code,'Waiting'), nvl(p_name,'Order'), p_p1, p_p2, p_p3, p_p4, p_p5, p_p6,
+w_lastdate, w_lasttime from tbl_worklist right outer join tbl_process using(w_workno) 
+left outer join tbl_product using(p_code) order by w_workno
 
 
 
