37b862f7b888be107b906f293f287605526c674c
yorutoitsuki <thouxcv@gmail.com>
2022-10-04 09:06:21 +0900

    Update processManagement.sql

--- 220923_Process_ManageMent/src/main/webapp/processManagement.sql
+++ 220923_Process_ManageMent/src/main/webapp/processManagement.sql
@@ -79,14 +79,14 @@ select * from tbl_process;
 select w_workno, P_code, p_name, p_size, p_type, w_quantity, to_char(w_workdate, 'yyyy-mm-dd')
 from tbl_product join tbl_worklist using(P_code) order by w_workno;
 
---14P SQL 작업공정조회
+--14P SQL 작업공정조회 - 작업 지시가 없는 공정은 조회 안함
 select w_workno, P_code, p_name, p_p1, p_p2, p_p3, p_p4, p_p5, p_p6,
 to_char(to_date(w_lastdate,'yyyy-mm-dd'),'yyyy-mm-dd'), w_lasttime 
 from tbl_worklist join tbl_process using(w_workno) 
 join tbl_product using(p_code) order by w_workno
 
 
---14P SQL 작업공정조회
+--14P SQL 작업공정조회 - 작업 지시가 없는 공정도 함께 조회, Waiting Order를 출력함
 select w_workno, nvl(P_code,'Waiting'), nvl(p_name,'Order'), p_p1, p_p2, p_p3, p_p4, p_p5, p_p6,
 to_char(to_date(w_lastdate,'yyyy-mm-dd'),'yyyy-mm-dd'), w_lasttime from tbl_worklist right outer join tbl_process using(w_workno) 
 left outer join tbl_product using(p_code) order by w_workno
