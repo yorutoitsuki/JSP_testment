5e9da14d3ab159d22b461acc68f9577be1454677
yorutoitsuki <thouxcv@gmail.com>
2022-10-07 12:25:48 +0900

    Create SalesBeans.java

---
+++ HRD_220221007_HomeShoping_JavaBean/src/main/java/member/SalesBeans.java
@@ -0,0 +1,34 @@
+package member;
+
+public class SalesBeans {
+	private String custno;
+	private String custname;
+	private String grade;
+	private String totalPrice;
+	public String getCustno() {
+		return custno;
+	}
+	public void setCustno(String custno) {
+		this.custno = custno;
+	}
+	public String getCustname() {
+		return custname;
+	}
+	public void setCustname(String custname) {
+		this.custname = custname;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(String totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	
+	
+}
